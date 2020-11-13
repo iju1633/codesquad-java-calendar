4dd84d4f4a2466cc9c2d7ba9184b63e1daae6f08
82102 <82102@DESKTOP-FS1L2S6>
2020-11-13 13:16:54 +0900

    update Calendar.java

--- src/eric/calendar/Calendar.java
+++ src/eric/calendar/Calendar.java
@@ -4,7 +4,13 @@ public class Calendar {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello Calendar");
+		System.out.println("일  월  화  수 목  금  토");
+		System.out.println("\n--------------------");
+		System.out.println("\n 1  2  3  4  5  6  7");
+		System.out.println("\n 8  9 10 11 12 13 14");
+		System.out.println("\n15 16 17 18 19 20 21");
+		System.out.println("\n22 23 24 25 26 27 28");
+		
 
 	}
 
