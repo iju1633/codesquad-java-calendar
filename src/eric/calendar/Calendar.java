8ba0f13a5a41e4f338068ec76d15cab5142301f3
82102 <82102@DESKTOP-FS1L2S6>
2020-11-13 14:01:42 +0900

    input 월은 ~일까지 있습니다. 함수

--- src/eric/calendar/Calendar.java
+++ src/eric/calendar/Calendar.java
@@ -1,9 +1,3 @@
-//		키보드로 입력받은 두 숫자의 합을 구한다.
-//		입력 및 출력 예시
-//		두 수를 입력하세요.
-//		5 10 (엔터)
-//		두 수의 합은 15입니다.
-
 package eric.calendar;
 import java.util.Scanner;
 
@@ -11,12 +5,33 @@ public class Calendar {
 
 	public static void main(String[] args) {
 		
-		System.out.println("두 수를 입력하세요.");
-		Scanner sc = new Scanner(System.in);
-		int num1 = sc.nextInt();
-		int num2 = sc.nextInt();
+		System.out.println("일  월  화  수 목  금  토\n--------------------");
 		
-		System.out.println("두 수의 합은 " + (num1+num2) + "입니다.");
+		System.out.println("\n 1  2  3  4  5  6  7");
+		System.out.println("\n 8  9 10 11 12 13 14");
+		System.out.println("\n15 16 17 18 19 20 21");
+		System.out.println("\n22 23 24 25 26 27 28");
+		
+//		달을 입력하세요.
+//		3 (엔터)
+//		3월은 31일까지 있습니다.
+		
+		System.out.println("달을 입력하세요");
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		int[] result = {31,28,31,30,31,30,31,31,30,31,30,31};
+		
+		if(input<1) {
+			System.out.println("1에서 12의 값을 입력해주십시오");
+		}
+		else if(input>12) {
+			System.out.println("1에서 12의 값을 입력해주십시오");
+		}
+		else {
+			System.out.printf("%d월은 %d일까지 있습니다.",input,result[input-1]);
+		}
+	
+		sc.close();
 		
 	}
 
