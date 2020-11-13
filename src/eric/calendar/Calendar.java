c9cd8b99f40decb102c15614589d032ad1c0932e
82102 <82102@DESKTOP-FS1L2S6>
2020-11-13 13:38:23 +0900

    Sum class

--- src/eric/calendar/Calendar.java
+++ src/eric/calendar/Calendar.java
@@ -1,17 +1,23 @@
+//		키보드로 입력받은 두 숫자의 합을 구한다.
+//		입력 및 출력 예시
+//		두 수를 입력하세요.
+//		5 10 (엔터)
+//		두 수의 합은 15입니다.
+
 package eric.calendar;
+import java.util.Scanner;
 
 public class Calendar {
 
 	public static void main(String[] args) {
 		
-		System.out.println("일  월  화  수 목  금  토");
-		System.out.println("\n--------------------");
-		System.out.println("\n 1  2  3  4  5  6  7");
-		System.out.println("\n 8  9 10 11 12 13 14");
-		System.out.println("\n15 16 17 18 19 20 21");
-		System.out.println("\n22 23 24 25 26 27 28");
+		System.out.println("두 수를 입력하세요.");
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
 		
-
+		System.out.println("두 수의 합은 " + (num1+num2) + "입니다.");
+		
 	}
 
 }
